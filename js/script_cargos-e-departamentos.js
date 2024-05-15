9094a27b17d0258ed7632556f680ebf6897ebe39
PedroA07 <pedro.andrade.pereira@hotmail.com.br>
2024-05-15 16:07:01 -0300

    Update

--- js/script_cargos-e-departamentos.js
+++ js/script_cargos-e-departamentos.js
@@ -1,3 +1,4 @@
+// Função para mostrar apenas as áreas necessárias do seletor correspondente
 function exibir_ocultar(val) {
     if(val.value == 'cargo_selecao') {
         document.getElementById('cargo_1_ativo').style.display = 'none';
@@ -26,52 +27,66 @@ function exibir_ocultar(val) {
     }
 };
 
+// Função para ocultar setores
 function exibir_ocultar_setor(val) {
-    const setorSelect = document.getElementById('setor');
-    const setorOptions = setorSelect.getElementsByTagName('option');
-  
-    // Redefinir o valor do elemento select setor
-    setorSelect.value = 'setor_selecao';
-  
-    // Ocultar todas as opções de setor
-    for (let i = 1; i < setorOptions.length; i++) {
+  const setorSelect = document.getElementById('setor');
+  const setorOptions = setorSelect.getElementsByTagName('option');
+
+  // Redefinir o valor do elemento select setor
+  setorSelect.value = 'setor_selecao';
+
+  // Ocultar todas as opções de setor
+  for (let i = 1; i < setorOptions.length; i++) {
       setorOptions[i].style.display = 'none';
-    }
-  
-    // Exibir opções de setor com base no departamento selecionado
-    if (val.value === 'administracao') {
+  }
+
+  // Variável para verificar se há algum setor visível
+  let setorVisivel = false;
+
+  // Exibir opções de setor com base no departamento selecionado
+  if (val.value === 'administracao') {
       document.getElementById('starq').style.display = 'block';
       document.getElementById('stpr').style.display = 'block';
       document.getElementById('stpd').style.display = 'block';
       document.getElementById('strh').style.display = 'block';
-    } else if (val.value === 'beneficios') {
+      setorVisivel = true;
+  } else if (val.value === 'beneficios') {
       document.getElementById('staf').style.display = 'block';
-    } else if (val.value === 'contabilidade') {
+      setorVisivel = true;
+  } else if (val.value === 'contabilidade') {
       // Não fazer nada
-    } else if (val.value === 'financas') {
+  } else if (val.value === 'financas') {
       document.getElementById('stte').style.display = 'block';
-    } else if (val.value === 'gestao_de_qualidade') {
+      setorVisivel = true;
+  } else if (val.value === 'gestao_de_qualidade') {
       // Não fazer nada
-    } else if (val.value === 'superintendencia') {
+  } else if (val.value === 'superintendencia') {
       document.getElementById('aj').style.display = 'block';
       document.getElementById('at').style.display = 'block';
-    } else if (val.value === 'materiais') {
+      setorVisivel = true;
+  } else if (val.value === 'materiais') {
       document.getElementById('stsg').style.display = 'block';
-    } else if (val.value === 'procuradoria_juridica') {
+      setorVisivel = true;
+  } else if (val.value === 'procuradoria_juridica') {
       // Não fazer nada
-    } else if (val.value === 'saude') {
+  } else if (val.value === 'saude') {
       document.getElementById('sas').style.display = 'block';
       document.getElementById('stod').style.display = 'block';
-    }
-  
-    // Exibir o elemento select setor
-    setorSelect.style.display = 'block';
+      setorVisivel = true;
+  }
 
-    // Selecionar a opção setor_selecao
-    for (let i = 0; i < setorOptions.length; i++) {
-        if (setorOptions[i].value === 'setor_selecao') {
-        setorOptions[i].selected = true;
-        break;
-        }
-    }
+  // Exibir o elemento select setor somente se houver algum setor visível
+  if (setorVisivel) {
+      setorSelect.style.display = 'block';
+  } else {
+      setorSelect.style.display = 'none';
+  }
+
+  // Selecionar a opção setor_selecao
+  for (let i = 0; i < setorOptions.length; i++) {
+      if (setorOptions[i].value === 'setor_selecao') {
+          setorOptions[i].selected = true;
+          break;
+      }
+  }
 };
--- js/script_formatacao-campos.js
+++ js/script_formatacao-campos.js
@@ -1,21 +1,52 @@
-// Máscara de CPF
-function mascara_cpf(i){
-   
+// Formatador do Celular
+const celMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
+// Formatador do Residencial
+const resdMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
+// Formatador da Matricula
+const matricula_Mask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"$1.$2")
+    value = value.replace(/(\d)(\d{1})$/,"$1-$2")
+    return value
+}
+// Formatador do CEP
+const cep_Mask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"$1 $2")
+    value = value.replace(/(\d)(\d{3})$/,"$1-$2")
+    return value
+}
+// Fim dos Formatadores
+
+// Manipulador de evento para input do CPF
+function mascaraCPFInput(i) {
     var v = i.value;
-    
-    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
-       i.value = v.substring(0, v.length-1);
-       return;
+
+    if (!/[0-9a-zA-Z]/.test(v[v.length - 1])) { // impede entrar outro caractere que não seja número ou letra
+        i.value = v.substring(0, v.length - 1);
+        return;
     }
-    
+
     i.setAttribute("maxlength", "14");
     if (v.length == 3 || v.length == 7) i.value += ".";
     if (v.length == 11) i.value += "-";
- 
-}
-
-// Máscara de RG
-function mascara_rg(i) {
+};
+// Manipulador de evento para input do RG
+function mascaraRGInput(i) {
     var v = i.value;
 
     if (!/[0-9a-zA-Z]/.test(v[v.length - 1])) { // impede entrar outro caractere que não seja número ou letra
@@ -26,72 +57,39 @@ function mascara_rg(i) {
     i.setAttribute("maxlength", "12");
     if (v.length == 2 || v.length == 6) i.value += ".";
     if (v.length == 10) i.value += "-";
-}
-// Máscara do NIT(PIS/PASEP)
-function mascara_nit(i) {
+};
+// Manipulador de evento para input do NIT(PIS/PASEP)
+function mascaraNITInput(i) {
     var v = i.value;
-    
+
     if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
        i.value = v.substring(0, v.length-1);
        return;
     }
     
-    i.setAttribute("maxlength", "17");
-    if (v.length == 2 || v.length == 6) i.value += ".";
-    if (v.length == 10) i.value += "/";
-    if (v.length == 15) i.value += "-";
-}
-
-// Máscara de Telefones
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 9) i.value += ".";
+    if (v.length == 12) i.value += "-";
+};
+// Manipulador de evento para input dos Telefones
 // Celular
 const mascara_telefone_celular = (event) => {
     let input = event.target
     input.value = celMask(input.value)
-  }
-  
-  const celMask = (value) => {
-    if (!value) return ""
-    value = value.replace(/\D/g,'')
-    value = value.replace(/(\d{2})(\d)/,"($1) $2")
-    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
-    return value
-}
+};
 // Residencial
 const mascara_telefone_residencial = (event) => {
     let input = event.target
     input.value = resdMask(input.value)
-  }
-  
-  const resdMask = (value) => {
-    if (!value) return ""
-    value = value.replace(/\D/g,'')
-    value = value.replace(/(\d{2})(\d)/,"($1) $2")
-    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
-    return value
-}
-// Máscara de Matrícula
-function mascara_matricula(i) {
-    var v = i.value;
-
-    if (isNaN(v[v.length - 1])) { // impede entrar outro caractere que não seja número
-        i.value = v.substring(0, v.length - 1);
-        return;
-    }
-
-    i.setAttribute("maxlength", "8");
-    if (v.length == 2) i.value += ".";
-    if (v.length == 6) i.value += "-";
-}
-// Máscara de CEP
-function mascara_cep(i) {
-    var v = i.value;
-
-    if (isNaN(v[v.length - 1])) { // impede entrar outro caractere que não seja número
-        i.value = v.substring(0, v.length - 1);
-        return;
-    }
-
-    i.setAttribute("maxlength", "10");
-    if (v.length == 2) i.value += ".";
-    if (v.length == 6) i.value += "-";
-}
+};
+// Manipulador de evento para input de Matrícula
+const mascara_matricula = (event) => {
+    let input = event.target
+    input.value = matricula_Mask(input.value)
+};
+// Manipulador de evento para input de CEP
+const mascara_cep = (event) => {
+    let input = event.target
+    input.value = cep_Mask(input.value)
+};
+// Fim dos Manipuladores de evento
